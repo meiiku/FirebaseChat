ff9eaad9be04bd3a5c304895d02aa64dbfe70b17
Fedor <f.b.fokin@gmail.com>
2022-12-14 23:01:00 +0300

    Initial commit

---
+++ FirebaseChat/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  FirebaseChat
+//
+//  Created by Fedor on 14.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .cyan
+    }
+}
+
